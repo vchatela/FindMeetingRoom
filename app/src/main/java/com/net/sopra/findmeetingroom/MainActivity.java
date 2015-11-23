40269b1a2905a0eab27ec115267334e5ba06e9a6
Matthieu De sousa <matthieu.desousa@gmail.com>
2015-11-23 15:53:15 +0100

    Commit main activity

--- app/src/main/java/com/net/sopra/findmeetingroom/MainActivity.java
+++ app/src/main/java/com/net/sopra/findmeetingroom/MainActivity.java
@@ -1,41 +1,21 @@
 package com.net.sopra.findmeetingroom;
 
+
+import android.app.Activity;
 // import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.Dialog;
-import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.app.AlertDialog;
+//import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity implements View.OnTouchListener, View.OnClickListener {
 
     private TextView b;
-    private final static int ID_DIALOG = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        b = (TextView) findViewById(R.id.about);
-        b.setOnClickListener(ablistener);
-    }
-
-    private OnClickListener ablistener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            showDialog(ID_DIALOG);
-        }
-    };
-
-    public Dialog onCreateDialog(int id) {
-        Dialog myD = new Dialog(this);
-        myD.setTitle("Informations");
-        return myD;
-    }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -44,5 +24,42 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
     @Override
     public void onClick(View v) {
+
+        // On récupère l'identifiant de la vue, et en fonction de cet identifiant…
+        switch(v.getId()) {
+
+            // Si l'identifiant de la vue est celui du premier bouton
+            case R.id.About:
+    /* Agir pour notre "label" */
+                this.showAbout();
+
+                break;
+            case R.id.connect:
+    /* Agir pour la pression du bouton connect*/
+
+
+                break;
+        }
     }
+
+    protected void showAbout() {
+        // Inflate the about message contents
+        View messageView = getLayoutInflater().inflate(R.layout.about, null, false);
+
+        // When linking text, force to always use default color. This works
+        // around a pressed color state bug.
+        TextView textView = (TextView) messageView.findViewById(R.id.about_credits);
+        int defaultColor = textView.getTextColors().getDefaultColor();
+        textView.setTextColor(defaultColor);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setTitle(R.string.app_name);
+        builder.setView(messageView);
+        builder.create();
+        builder.show();
+
+    }
+
+
 }
