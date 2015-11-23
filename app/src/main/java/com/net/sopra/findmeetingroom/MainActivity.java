801b5c2e186668600010ea5035aa90e5c507fb16
pierre <pierromumu@gmail.com>
2015-11-23 15:30:31 +0100

    common version (after merging both Matthieu's and Pierre's work)

--- app/src/main/java/com/net/sopra/findmeetingroom/MainActivity.java
+++ app/src/main/java/com/net/sopra/findmeetingroom/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity implements View.OnTouchListener, View.OnClickListener {
 
     private TextView b;
+    private final static int ID_DIALOG = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,20 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
         setContentView(R.layout.activity_main);
 
         b = (TextView) findViewById(R.id.about);
-        b.setOnTouchListener(this);
-        b.setOnClickListener(this);
+        b.setOnClickListener(ablistener);
+    }
+
+    private OnClickListener ablistener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDialog(ID_DIALOG);
+        }
+    };
+
+    public Dialog onCreateDialog(int id) {
+        Dialog myD = new Dialog(this);
+        myD.setTitle("Informations");
+        return myD;
     }
 
     @Override
@@ -30,15 +44,6 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
     @Override
     public void onClick(View v) {
-
-        // getting the view id and reacting accordingly
-        switch(v.getId()) {
-
-            case R.id.about:
-                break;
-
-            case R.id.connect:
-                break;
         }
     }
 }
