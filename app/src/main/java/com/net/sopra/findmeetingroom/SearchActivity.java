1daee43980100a6c3f4eafe643a2e9ae58c1f8bc
pierre <pierromumu@gmail.com>
2015-11-23 16:43:51 +0100

    common version (after merging both Matthieu's and Pierre's work)

---
+++ app/src/main/java/com/net/sopra/findmeetingroom/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.net.sopra.findmeetingroom;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+    }
+
+}
