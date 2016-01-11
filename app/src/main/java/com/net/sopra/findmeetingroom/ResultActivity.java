39f1d1fe990eaa3bd39fe669ee5ff9c5e327b865
pierre <pierromumu@gmail.com>
2016-01-11 17:02:05 +0100

    version 11/01/16 (vérifications supplémentaires sur les critères de recherche)

--- app/src/main/java/com/net/sopra/findmeetingroom/ResultActivity.java
+++ app/src/main/java/com/net/sopra/findmeetingroom/ResultActivity.java
@@ -24,28 +24,38 @@ public class ResultActivity extends Activity {
 
         // POUR LES TESTS UNIQUEMENT
         baseResult = "[{\"ID\":1,\"idBuilding\":1,\"nom\":\"N1\",\"etage\":0},{\"ID\":2,\"idBuilding\":1,\"nom\":\"N2\",\"etage\":1},{\"ID\":3,\"idBuilding\":1,\"nom\":\"N3\",\"etage\":2}]";
+        //baseResult = "";
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results_activity);
 
-        StringReader reader = new StringReader(baseResult);
-        Gson gson = new Gson();
-        Type typeR = new TypeToken<ArrayList<Room>>() {}.getType();
-        ArrayList<Room> roomsList = gson.fromJson(reader, typeR);
-        int nbR = roomsList.size();
-        String[] partsR = new String[nbR];
+        String[] partsR;
 
-        int i=0;
-        Iterator<Room> itR = roomsList.iterator();
-        Room tempR;
-        while (itR.hasNext()) {
-            tempR = itR.next();
-            partsR[i] = tempR.getnom()+" ("+String.valueOf(tempR.getetage())+")";
-            i++;
+        if(baseResult==null || baseResult=="") {
+            partsR = new String[1];
+            partsR[0] = getResources().getString(R.string.aures);
+        }
+        else {
+            StringReader reader = new StringReader(baseResult);
+            Gson gson = new Gson();
+            Type typeR = new TypeToken<ArrayList<Room>>() {
+            }.getType();
+            ArrayList<Room> roomsList = gson.fromJson(reader, typeR);
+            int nbR = roomsList.size();
+            partsR = new String[nbR];
+
+            int i = 0;
+            Iterator<Room> itR = roomsList.iterator();
+            Room tempR;
+            while (itR.hasNext()) {
+                tempR = itR.next();
+                partsR[i] = tempR.getnom() + " (" + String.valueOf(tempR.getetage()) + ")";
+                i++;
+            }
         }
 
         lv = (ListView) findViewById(R.id.resultsList);
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, partsR);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, partsR);
         lv.setAdapter(adapter);
     }
 
